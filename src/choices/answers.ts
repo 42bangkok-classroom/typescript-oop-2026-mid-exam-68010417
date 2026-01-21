7dbbaef719d2915d6b5be0cdda5933e37e973093
68010417 <68010417@kmitl.ac.th>
2026-01-21 15:15:37 +0700

    p03_1and_fix_c01

--- src/choices/answers.ts
+++ src/choices/answers.ts
@@ -4,7 +4,7 @@
  * exmaple { "c01": "A" }
  */
 export const answers = {
-  "c01": "B",
+  "c01": "A",
   "c02": "B",
   "c03": "C",
   "c04": "B",
--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,20 @@
 // Write your code below
+let input = process.argv[2];
+let input_2 = Number(input)
+
+if (Number.isNaN(input_2) || input_2 <= 0 || !Number.isInteger(input_2) || process.argv[2] === "") {
+    console.log('Invalid Input');
+    process.exit();
+}
+
+for (let i = 1; i <= input_2; i++){
+  if (i % 7 === 0 && i % 3 === 0){
+    console.log('FizzBuzz');
+  }else if (i % 3 === 0){
+    console.log('Fizz');
+  }else if (i % 7 === 0){
+    console.log('Buzz');
+  }else if (!(i % 3 === 0 || i % 7 === 0)){
+    console.log(`${i}`);
+  }
+}
