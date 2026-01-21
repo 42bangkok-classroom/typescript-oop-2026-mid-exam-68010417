f7d22f03bdb5d1c189faf1244a30aefd2b723c7a
68010417 <68010417@kmitl.ac.th>
2026-01-21 15:21:20 +0700

    p03_2

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -3,7 +3,7 @@ let input = process.argv[2];
 let input_2 = Number(input)
 
 if (Number.isNaN(input_2) || input_2 <= 0 || !Number.isInteger(input_2) || process.argv[2] === "") {
-    console.log('Invalid Input');
+    
 }else{
 for (let i = 1; i <= input_2; i++){
   if (i % 7 === 0 && i % 3 === 0){
