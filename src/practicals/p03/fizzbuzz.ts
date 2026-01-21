5dffdf9c036cc1f05f734143ec632b8f2c6f760d
68010417 <68010417@kmitl.ac.th>
2026-01-21 15:25:24 +0700

    p03_5

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -5,13 +5,13 @@ if (Number.isNaN(input) || input <= 0 || !Number.isInteger(input) || process.arg
     
 }else{
 for (let i = 1; i <= input; i++){
-  if (i % 7 === 0 && i % 3 === 0){
+  if (i % 5 === 0 && i % 3 === 0){
     console.log('FizzBuzz');
   }else if (i % 3 === 0){
     console.log('Fizz');
-  }else if (i % 7 === 0){
+  }else if (i % 5 === 0){
     console.log('Buzz');
-  }else if (!(i % 3 === 0 || i % 7 === 0)){
+  }else if (!(i % 3 === 0 || i % 5 === 0)){
     console.log(`${i}`);
   }
 }
