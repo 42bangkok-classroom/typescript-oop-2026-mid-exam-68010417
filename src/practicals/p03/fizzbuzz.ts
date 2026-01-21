83f0b1e224b55f6aa977a0142c6383ae2f71caac
68010417 <68010417@kmitl.ac.th>
2026-01-21 15:24:25 +0700

    p03_4

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,11 +1,10 @@
 // Write your code below
-let input = process.argv[2];
-let input_2 = Number(input)
+let input = +process.argv[2];
 
-if (Number.isNaN(input_2) || input_2 <= 0 || !Number.isInteger(input_2) || process.argv[2] === "") {
+if (Number.isNaN(input) || input <= 0 || !Number.isInteger(input) || process.argv[2] === "") {
     
 }else{
-for (let i = 1; i <= input_2; i++){
+for (let i = 1; i <= input; i++){
   if (i % 7 === 0 && i % 3 === 0){
     console.log('FizzBuzz');
   }else if (i % 3 === 0){
